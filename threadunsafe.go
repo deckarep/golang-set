d6cb77c3f0b7225ab173c1db4a7cf4a534e9ab8b
Ralph Caraveo <deckarep@gmail.com>
2023-03-05 11:27:51 -0800

    Fixing how Append gets dereferenced due to some recent changes.
    We no longer need two layers of indirection.

--- threadunsafe.go
+++ threadunsafe.go
@@ -47,12 +47,12 @@ func (s threadUnsafeSet[T]) Add(v T) bool {
 	return prevLen != len(s)
 }
 
-func (s *threadUnsafeSet[T]) Append(v ...T) int {
-	prevLen := len(*s)
+func (s threadUnsafeSet[T]) Append(v ...T) int {
+	prevLen := len(s)
 	for _, val := range v {
-		(*s)[val] = struct{}{}
+		(s)[val] = struct{}{}
 	}
-	return len(*s) - prevLen
+	return len(s) - prevLen
 }
 
 // private version of Add which doesn't return a value
