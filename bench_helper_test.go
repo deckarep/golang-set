fa6bbb98526d93db0109dc4439dc2d0e7ad1d081
Hiroyuki Toda <mai7star@gmail.com>
2026-05-05 12:20:54 +0900

    call json.Marshal() directly

---
+++ bench_helper_test.go
@@ -0,0 +1,49 @@
+package mapset
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func nrand(n int) []int {
+	is := make([]int, n)
+	for i := range is {
+		is[i] = rand.Int()
+	}
+	return is
+}
+
+func buildRandomSet(n int, safe bool) Set[int] {
+	if safe {
+		return NewSet(nrand(n)...)
+	} else {
+		return NewThreadUnsafeSet(nrand(n)...)
+	}
+}
+
+func buildCaseName(n int, safe bool) string {
+	var settype string
+	if safe {
+		settype = "Safe"
+	} else {
+		settype = "Unsafe"
+	}
+
+	return fmt.Sprintf("%s(%d)", settype, n)
+}
+
+type benchCase struct {
+	n    int
+	safe bool
+}
+
+func buildBenchCases(ns ...int) []benchCase {
+	cases := []benchCase{}
+	for _, n := range ns {
+		cases = append(cases, benchCase{n, true})
+	}
+	for _, n := range ns {
+		cases = append(cases, benchCase{n, false})
+	}
+	return cases
+}
